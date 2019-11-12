bcae0e60ecc8792b86ca14517f80da7d7bc37fda
takase <takase@mitsuwa-off.jp>
2019-11-12 12:11:20 +0900

    customeredit2

--- test/src/main/java/com/example/demo/repository/CustomerTelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.CustomerTel;
-
-public interface CustomerTelRepository extends JpaRepository<CustomerTel, Integer> {
-
-}
