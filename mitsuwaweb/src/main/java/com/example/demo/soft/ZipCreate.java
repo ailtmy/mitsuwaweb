5331c490081f29ceeab3cbdb34ac054b811ccc14
takase <takase@mitsuwa-off.jp>
2019-12-10 12:56:18 +0900

    oshirase kobunsyo soft

---
+++ mitsuwaweb/src/main/java/com/example/demo/soft/ZipCreate.java
@@ -0,0 +1,94 @@
+package com.example.demo.soft;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class ZipCreate {
+
+	public static void compressDirectory(final File destination, final File dir)
+			throws IOException {
+
+		// 変数宣言
+		byte[] buf = new byte[1024];
+		ZipOutputStream zos = null;
+		InputStream is = null;
+
+		// ZIP対象フォルダ配下の全ファイルを取得
+		List<File> files = new ArrayList<File>();
+		getFiles(dir, files);
+
+		try {
+
+			// ZIP出力オブジェクトを取得（日本語の文字化けに対応するために文字コードは Shift-JIS を指定）
+			zos = new ZipOutputStream(
+					new BufferedOutputStream(new FileOutputStream(destination)), Charset.forName("Shift-JIS"));
+
+			// 全ファイルをZIPに格納
+			for (File file : files) {
+
+				// ZIP化実施ファイルの情報をオブジェクトに設定
+				ZipEntry entry = new ZipEntry(
+						file.getAbsolutePath().replace(dir.getAbsolutePath() + File.separator, ""));
+				zos.putNextEntry(entry);
+
+
+				// ZIPファイルに情報を書き込む
+				is = new BufferedInputStream(new FileInputStream(file));
+				int len = 0;
+				while ((len = is.read(buf)) != -1) {
+					zos.write(buf, 0, len);
+				}
+
+				// ストリームを閉じる
+				is.close();
+			}
+
+			// 処理の最後にストリームは常に閉じる
+		} finally {
+			if (zos != null) {
+				zos.close();
+			}
+			if (is != null) {
+				is.close();
+			}
+		}
+	}
+
+	/**
+	 * 指定したフォルダ配下の全ファイルを取得
+	 * @param parentDir ファイル取得対象フォルダ
+	 * @param files     ファイル一覧
+	 */
+	public static void getFiles(final File parentDir, final List<File> files) {
+
+		// ファイル取得対象フォルダ直下のファイル,ディレクトリを走査
+		for (File f : parentDir.listFiles()) {
+
+			// ファイルの場合はファイル一覧に追加
+			if (f.isFile()) {
+				files.add(f);
+
+			} else if(f.isDirectory() && f.listFiles() == null) {
+
+				files.add(f);
+
+
+				// ディレクトリの場合は再帰処理
+			} else if (f.isDirectory() ) {
+				getFiles(f, files);
+
+			}
+
+		}
+	}
+}
--- mitsuwaweb/src/main/java/com/example/demo/soft/service/MishikkoService.java
+++ mitsuwaweb/src/main/java/com/example/demo/soft/service/MishikkoService.java
@@ -23,6 +23,7 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import com.example.demo.soft.ZipCreate;
 import com.example.demo.soft.entity.Mishikko;
 import com.example.demo.soft.repository.MishikkoRepository;
 
@@ -46,6 +47,7 @@ public class MishikkoService {
 		Document xsldocument = builder.parse(Paths.get("./src/main/resources/hinagata/mishikko/syomei/HM0504000300001.xsl").toFile());
 		Document rdfdocument = builder.parse(Paths.get("./src/main/resources/hinagata/mishikko/syomei/index.rdf").toFile());
 		Document infodocument = builder.parse(Paths.get("./src/main/resources/hinagata/mishikko/info.xml").toFile());
+		Document exportdocument = builder.parse(Paths.get("./export.xml").toFile());
 		// 4. Documentから、ルート要素(mishikkoElement)を取得する
 		Element mishikkoElement = document.getDocumentElement();
 
@@ -54,12 +56,12 @@ public class MishikkoService {
 
 		NodeList tokisyoName = mishikkoElement.getElementsByTagName("宛先登記所名");
 		NodeList teisyutusakiName = mishikkoElement.getElementsByTagName("提出先名称");
-		tokisyoName.item(0).setTextContent("大阪法務局");
-		teisyutusakiName.item(0).setTextContent("大阪法務局");
+		tokisyoName.item(0).setTextContent("奈良地方法務局");
+		teisyutusakiName.item(0).setTextContent("奈良地方法務局");
 		NodeList tokisyoCode = mishikkoElement.getElementsByTagName("登記所コード");
 		NodeList teisyutusakiCode = mishikkoElement.getElementsByTagName("提出先コード");
-		tokisyoCode.item(0).setTextContent("1200");
-		teisyutusakiCode.item(0).setTextContent("1200");
+		tokisyoCode.item(0).setTextContent("1500");
+		teisyutusakiCode.item(0).setTextContent("1500");
 
 
 		//物件追加の場合
@@ -78,8 +80,8 @@ public class MishikkoService {
 
 		for(int i = 0; i < bukkenKubun.getLength(); i++) {
 			bukkenKubun.item(i).setTextContent("建物");
-			chibanKuiki.item(i).setTextContent("大阪市中央区備後町四丁目");
-			chibanKaokubango.item(i).setTextContent("１－１");
+			chibanKuiki.item(i).setTextContent("奈良市東九条町");
+			chibanKaokubango.item(i).setTextContent("１０１４－１４－２");
 			mokuteki.item(i).setTextContent("抵当権設定");
 			yoshiKubun.item(i).setTextContent("乙区");
 			uketsukeDate.item(i+1).setTextContent("令和１年１２月１日");
@@ -91,22 +93,42 @@ public class MishikkoService {
 
         tf.setOutputProperty("indent", "yes");
         tf.setOutputProperty("encoding", "UTF-8");
-
+        File togo = new File("./統合前");
         File export = new File("./export");
         File kanri = new File("./export/署名・送信");
-        File oshirase = new File("./export/お知らせ");
-        File kobunsyo = new File("./export/取得公文書");
+        File oshirase = new File("./export/お知らせ/");
+
+        File kobunsyo = new File("./export/取得公文書/");
+
         File htmlFile = new File("./export/HM0504000300001.html");
+        togo.mkdir();
         export.mkdir();
         kanri.mkdir();
         oshirase.mkdir();
         kobunsyo.mkdir();
+
         htmlFile.createNewFile();
         tf.transform(new DOMSource(document), new StreamResult(new File("./export/署名・送信/HM0504000300001.xml")));
         tf.transform(new DOMSource(xsldocument), new StreamResult(new File("./export/署名・送信/HM0504000300001.xsl")));
         tf.transform(new DOMSource(rdfdocument), new StreamResult(new File("./export/署名・送信/index.rdf")));
         tf.transform(new DOMSource(infodocument), new StreamResult(new File("./export/info.xml")));
+        tf.transform(new DOMSource(exportdocument), new StreamResult(new File("./統合前/export.xml")));
 
+
+        // ZIP化実施フォルダを取得
+     	File destination = new File("./統合前/テスト未失効.zip");
+
+     	// 圧縮実行
+     	ZipCreate.compressDirectory(destination, export);
+
+     	// ZIP化実施フォルダを取得
+     	File dir = new File("./統合前");
+
+     	// ZIP化実施フォルダを取得
+     	File destination2 = new File("./申請ファイル.zip");
+
+     	// 圧縮実行
+     	ZipCreate.compressDirectory(destination2, dir);
 
 
 		return mishikkoElement;
